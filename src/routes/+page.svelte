3f87c571f48b84179e78fa40ce43ce68a4617eb0
jeremt <taboada.jeremie@gmail.com>
2026-01-10 12:46:14 +0100

    fix: add scenes into the website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,6 +8,19 @@
 			.values()
 			.find(([actor, characters]) => characters.includes(character))?.[0];
 	};
+	const scenes = $derived.by(() => {
+		const scenes: number[] = [];
+		for (let i = 0; i < data.dialogs.length; i++) {
+			if (
+				data.dialogs[i].dialogs.find(
+					(dialog) => dialog.type === 'dialog' && getActor(dialog.character) === selectedActor
+				)
+			) {
+				scenes.push(i + 1);
+			}
+		}
+		return scenes;
+	});
 </script>
 
 <header>
@@ -26,11 +39,16 @@
 			<option value="color">Surligner</option>
 		</select>
 	</label>
+	{#each scenes as scene}<a href="#{scene}" class="text-blue-900">#{scene}</a>{/each}
 </header>
 
 <div class="p-4 font-serif">
-	{#each data.dialogs as scene}
-		<h2 class="py-8 text-xl font-bold text-blue-900">{scene.title}</h2>
+	{#each data.dialogs as scene, index}
+		<a href="#{index + 1}"
+			><h2 id={(index + 1).toString()} class="py-8 text-xl font-bold text-blue-900">
+				{scene.title}
+			</h2></a
+		>
 		<div class="flex flex-col gap-4">
 			{#each scene.dialogs as dialog}
 				{#if dialog.type === 'didascalie'}
